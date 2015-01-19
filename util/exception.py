b33b4f40c9f117f6ca2708b2763d6de3571d1529
xuxingtiancai <xuxingtiancai@gmail.com>
2015-01-19 15:36:00 +0800

    Update exception.py

--- util/exception.py
+++ util/exception.py
@@ -1,5 +1,17 @@
+#错误记录
+try:  
+    print test1.division(10, 0)  
+except:  
+    print 'invoking division failed.'  
+    traceback.print_exc()  
+    sys.exit(1)  
+    
+logging.exception(ex)
+logging.error(ex, exc_info=1) # 指名输出栈踪迹, logging.exception的内部也是包了一层此做法
+logging.critical(ex, exc_info=1) # 更加严重的错误级别
 
 
+#Exception类别
 class MyException(Exception):  
     def __init__(self, msg):  
         Exception.__init__(self, msg)
