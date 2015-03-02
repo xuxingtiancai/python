349a9e4b17276fdd3d092b35ffe1ce1bc415caa9
xuxingtiancai <xuxingtiancai@gmail.com>
2015-03-02 18:11:10 +0800

    Update memo.py

--- decorate/memo.py
+++ decorate/memo.py
@@ -2,7 +2,6 @@ from functools import wraps
 def memo(fn):
     @wraps(fn)
     def wrapper(*args):
-        print len(wrapper.cache)
         result = wrapper.cache.get(args)
         if result is None:
             result = fn(*args)
