e19e14414c263c288b09fe8e045c24c7818906b0
xuxingtiancai <xuxingtiancai@gmail.com>
2015-03-04 12:05:51 +0800

    Update memo.py

--- decorate/memo.py
+++ decorate/memo.py
@@ -9,3 +9,20 @@ def memo(fn):
         return result
     wrapper.cache = {}
     return wrapper
+    
+def list_memo(fn):
+    @wraps(fn)
+    def wrapper(list_arg):
+        cache_result = []
+        miss_arg = []
+        for arg in list_arg:
+            if arg in wrapper.cache:
+                cache_result.append(wrapper.cache[arg])
+            else:
+                miss_arg.append(arg)
+        if not list_arg:
+            return cache_result
+        
+        fn_result = fn(miss_arg)
+        wrapper.cache.update(dict(zip(miss_arg, fn_result)))
+        return cache_result + fn_result
