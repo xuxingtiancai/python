2859dc5d5ee53c278182b30c0726d62cd564a35b
xuxingtiancai <xuxingtiancai@gmail.com>
2017-03-06 16:16:23 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,8 +1,8 @@
 def deco(arg):  
-def _deco(func):  
-def __deco():  
-print("before %s called [%s]." % (func.__name__, arg))  
-func()  
-print("  after %s called [%s]." % (func.__name__, arg))  
-return __deco  
-return _deco
+  def _deco(func):  
+    def __deco():  
+      print("before %s called [%s]." % (func.__name__, arg))  
+      func()  
+      print("  after %s called [%s]." % (func.__name__, arg))  
+    return __deco  
+  return _deco
