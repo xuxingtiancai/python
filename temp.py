766b3caf4394c3d8c360d4cee0cccddca6ade364
xuxingtiancai <xuxingtiancai@gmail.com>
2017-03-06 16:18:47 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,9 @@
-class dec:
-    def __init__(self, f):
-        self.f = f
-    def __call__(self, *args):
-        print "Decorating", self.f.__name__
-        self.f(*args)
+def decro(cls):
+    def func(self):
+        print 'hello world'
+    cls.func = func
+    return cls
 
-@dec
-def f(a, b):
-print "f", a, b
+@decro
+class A:
+    pass
