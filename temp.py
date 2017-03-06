f71116ae0bf517967b236c817d2d65e97362c2cc
xuxingtiancai <xuxingtiancai@gmail.com>
2017-03-06 16:23:03 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,6 +1,8 @@
-def f():
-    for i in range(5):
-	yield i
-	
-for i in f():
-	print i
+obj and obj.value
+obj or list()
+
+for i in range(5):
+    print i
+else:
+    print ‘Iterated over everything’
+
