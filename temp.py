5d28850927ea745d397dc1713fcf669440b131c1
xuxingtiancai <xuxingtiancai@gmail.com>
2017-03-06 16:22:16 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,7 +1,6 @@
-def perms(elements):
-    if len(elements) <=1:
-	yield elements
-    else:
-	for perm in perms(elements[1:]):
-            for i in range(len(elements)):
-		yield perm[:i] + elements[0:1] + perm[i:]
+def f():
+    for i in range(5):
+	yield i
+	
+for i in f():
+	print i
